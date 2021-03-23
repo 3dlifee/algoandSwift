3f45272954e2381d39ff2b3344c17c912cffb297
3dlifeStudio <mjfernandez3d@gmail.com>
2021-03-23 17:26:49 +0000

    Initial Commit

---
+++ algoandSwift/algoandSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  algoandSwiftApp.swift
+//  algoandSwift
+//
+//  Created by Mario Fernandes on 23/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct algoandSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
